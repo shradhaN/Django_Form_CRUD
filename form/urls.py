4a519d8ad4accf655cbbac751e982677d551aa96
Shradha Neupane <shradha@vacker360.com>
2018-07-07 17:05:27 +0545

    open list page first

--- form/urls.py
+++ form/urls.py
@@ -8,7 +8,7 @@ from form import views
 urlpatterns = [
 
 	url(r'^get_book/$', views.get_book, name='book'),
-	url(r'^done/$', views.done,name="done"),
+	url(r'^$', views.done,name="done"),
 	url(r'^edit/(?P<id>[\d]+)/$', views.book_edit, name="edit"),
 	url(r'^delete/(?P<id>[\d]+)/$', views.delete, name="delete")
 
